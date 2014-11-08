59ad1c2465b34fce20f1db5794c3df926cfddc77
Garrett Amini <kadjar@gmail.com>
2014-11-07 17:48:48 -0800

    adding ipsums

---
+++ config/config.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+config :text,
+  flavors: ["lorem_ipsum", "iliad", "frankenstein", "lorem_ipsum", "riker", "cat"],
+  flavor: "lorem_ipsum",
+  format: "text "
--- test/test_api.exs
+++ test/test_api.exs
@@ -1,5 +1,22 @@
 defmodule Tests.Api do
   use ExUnit.Case
+  use Elixilorem
 
+  test "GetSum: returns one long string of unformatted Lorem Ipsum" do
+    # assert is_binary(GetSum)
+    # assert String.length(GetSum) > 10
+  end
+
+  test "GetSum/1: returns x paragraphs of unformatted Lorem Ipsum" do
+    #assert GetSum(3) has two line breaks
+  end
+
+  test "GetSum can get x paragraphs of text in a specified format" do
+    # assert GetSum(3, {format: "html"})
+  end
+
+  test "GetSum can get x paragraphs of text from a specified flavor" do
+    # assert GetSum(3, {flavor: "cat"})
+  end
 
 end
